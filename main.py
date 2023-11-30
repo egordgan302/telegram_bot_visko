8ccf878010b66feb197ee95d761991ba9d96288b
egor <troyanviskov@gmail.com>
2023-11-30 22:27:07 +0300

    bot in telebot

--- main.py
+++ main.py
@@ -1,60 +1,27 @@
-from aiogram import Bot, types, Dispatcher
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils import executor
-
-bot = Bot(token="")
-dp = Dispatcher(bot)
-try:
-    with open("sp_gr.txt", "r", encoding="utf-8") as file:
-        students = [line.rstrip("\n") for line in file]
-        students= list(filter(None, students))
-except FileNotFoundError:
-    print(f"Файл" 'sp_gr.txt' "не найден")
-
-try:
-    with open("zadaniya.txt", "r", encoding="utf-8") as file:
-        zadaniya = [line.rstrip("\n") for line in file]
-        zadaniya= list(filter(None, zadaniya))
-except FileNotFoundError:
-    print(f"Файл" 'zadaniya.txt' "не найден")
-
-@dp.message_handler(commands=["start"])
-async def start_command(message: types.Message):
-    buttons = [
-        [types.KeyboardButton(text="Help")],
-        [types.KeyboardButton(text="Что-то")]
-               ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=buttons,  resize_keyboard=True)
-
-    await message.answer(text="Hello, <em>in absurd world</em>", parse_mode="HTML", reply_markup=keyboard)
-
-HELP_COMMAND = """Этот бот даст задание каждому студенту на зачет сделав это в состоянии абсурда под дозой
-/help - список команд
-/start - начать работу"""
-@dp.message_handler(lambda message: message.text == "Help")
-async def help1_command(message: types.Message):
-    await message.answer(text=HELP_COMMAND, parse_mode="HTML")
-    await message.delete()
-    
-@dp.message_handler(commands=["help"])
-async def help2_command(message: types.Message):
-    await message.answer(text=HELP_COMMAND, parse_mode="HTML")
+import telebot
+from telebot import types
+import random
 
 
-@dp.message_handler()
-async def zadaniya(message:types.Message):
-    import random
+bot = telebot.TeleBot("")
 
-    a=random.choice(zadaniya)
-    found=False
-    for name in students:
-        if name.startswith(message.text):
-            found=True
-            break
-    if found:
-        await message.answer(text=f"Задание....: {a}")
-    else:
-        await message.answer(text="ты лишний, абсурда не будет")
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("Рандомное число"))
+    markup.add(types.KeyboardButton("Рандомный напиток"))
+    bot.send_message(message.chat.id, "Выберите действие", reply_markup=markup)
 
-if __name__ == "main":
-    executor.start_polling(dp, skip_updates=True)
+@bot.message_handler(func=lambda message: message.text.lower() == "рандомный напиток")
+def random_drink(message):
+    drinks = ["Вода", "Святая вода", "Кола", "Пивко", "Виски"]
+    random_drink = random.choice(drinks)
+    bot.send_message(message.chat.id, f"Рандомный напиток: {random_drink}")
+
+@bot.message_handler(func=lambda message: message.text.lower() == "рандомное число")
+def random_number(message):
+    random_number = random.randint(-100000, 100000)
+    bot.send_message(message.chat.id, f"Рандомное число: {random_number}")
+
+
+bot.polling(none_stop=True)
